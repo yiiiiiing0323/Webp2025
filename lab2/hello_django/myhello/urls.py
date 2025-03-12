5726aeda08c633b6d45258f6e82b1e07f355c5a9
yiiiiiing0323 <146087320+yiiiiiing0323@users.noreply.github.com>
2025-03-12 15:08:26 +0800

    Add files via upload

---
+++ lab2/hello_django/myhello/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    #path('',views.HelloApiView.as_view(),name='index'),
+    #path('',views.myhello_api,name='index'),
+    path('add', views.add_post, name='add_post'),
+    path('list', views.list_post, name='list_post')
+]
---
+++ lab2/hello_django/myhello/views.py
@@ -0,0 +1,74 @@
+#from django.shortcuts import render
+#from django.http import HttpResponse
+# Create your views here.
+#from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from django.core.serializers.json import DjangoJSONEncoder
+import json
+import logging
+from .models import Post
+'''def myIndex(request):
+    my_name = request.GET.get('name', "CGU")  
+    return HttpResponse("Hello " + my_name)'''
+logger =logging.getLogger('django')
+@api_view(['GET'])
+def add_post(request):
+    title =request.GET.get('title','')
+    content =request.GET.get('content','')
+    photo= request.GET.get('photo','')
+    location=request.GET.get('location','')
+
+    new_post=Post()
+    new_post.title=title
+    new_post.content=content
+    new_post.photo=photo
+    new_post.location=location
+    new_post.save()
+    logger.debug("*************** myhello_api: " + title)
+    if title:
+        return Response({"data": title + " insert!"},status.HTTP_200_OK)
+    else:
+        return Response(
+            {"res": "parameter: name is None"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+@api_view(['GET'])
+def list_post(request):
+    posts= Post.objects.all().values()
+    return JsonResponse(list(posts), safe=False)
+    '''return Response({"data":
+                     json.dumps(
+                         list(posts),
+                         sort_keys=True,
+                         indent= 1,
+                         cls=DjangoJSONEncoder)},
+                    status=status.HTTP_200_OK) '''
+                     
+
+
+'''def myhello_api(request):
+    my_name =request.GET.get('name',None)
+    if my_name:
+        return Response({"data":"Hello " + my_name}, status=status.HTTP_200_OK)
+    else:
+        return Response(
+            {"res":"parameter: name is None"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+          
+class HelloApiView(APIView):
+    def get(self,request):
+        my_name = request.GET.get('name' , None)
+        if my_name:
+            retValue= {}
+            retValue['data'] = "Hello" +my_name
+            return Response(retValue, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {"res":"parameter: name is None"},
+                status=status.HTTP_400_BAD_REQUEST
+            )'''
+       
